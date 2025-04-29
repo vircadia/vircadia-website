46d0bacbfcc2dc47cea76a6f1859bd93a355a33f
Kalila <69767640+digisomni@users.noreply.github.com>
2025-04-30 02:30:23 +0800

    WIP, setting up website for launch.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,151 +1,134 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Vircadia World | Declarative MMO Game Framework',
-  tagline: 'Declarative MMO Game Framework',
-  favicon: 'img/favicon.ico',
+	title: "Vircadia",
+	tagline: "Worlds made easy.",
+	favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: 'https://vircadia.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+	// Set the production url of your site here
+	url: "https://vircadia.com",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Vircadia', // Usually your GitHub org/user name.
-  projectName: 'vircadia-world', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "Vircadia", // Usually your GitHub org/user name.
+	projectName: "vircadia-world", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          path: 'docs/vircadia-world/',
-          routeBasePath: 'vircadia-world',
-          exclude: [
-            'cli/vircadia-world-sdk-ts/**',
-            'client/web_babylon_js/volume/vircadia-world-sdk-ts/**',
-            'server/service/api/volume/vircadia-world-sdk-ts/**',
-            'server/service/tick/volume/vircadia-world-sdk-ts/**'
-          ],
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Vircadia/vircadia-website/tree/master/docs/vircadia-world/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Vircadia/vircadia-website/tree/master/blog/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					path: "docs/vircadia-world/",
+					routeBasePath: "vircadia-world",
+					exclude: [
+						"cli/vircadia-world-sdk-ts/**",
+						"client/web_babylon_js/volume/vircadia-world-sdk-ts/**",
+						"server/service/api/volume/vircadia-world-sdk-ts/**",
+						"server/service/tick/volume/vircadia-world-sdk-ts/**",
+					],
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/Vircadia/vircadia-website/tree/master/docs/vircadia-world/",
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ["rss", "atom"],
+						xslt: true,
+					},
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/Vircadia/vircadia-website/tree/master/blog/",
+					// Useful options to enforce blogging best practices
+					onInlineTags: "warn",
+					onInlineAuthors: "warn",
+					onUntruncatedBlogPosts: "warn",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Vircadia`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		// Replace with your project's social card
+		image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "Vircadia",
+			logo: {
+				alt: "Vircadia Logo",
+				src: "img/icon.svg",
+			},
+			items: [
+				{ to: "/vircadia-world", label: "Documentation", position: "left" },
+				{
+					to: "/vircadia-world/cli/#quick-start",
+					label: "Quick Start",
+					position: "left",
+				},
+				{ to: "/blog", label: "Blog", position: "left" },
+				{
+					href: "https://github.com/Vircadia",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Community",
+					items: [
+						{
+							label: "Discord",
+							href: "https://discordapp.com/invite/Pvx2vke",
+						},
+						{
+							label: "X",
+							href: "https://x.com/vircadia",
+						},
+					],
+				},
+				{
+					title: "Code",
+					items: [
+						{
+							label: "GitHub",
+							href: "https://github.com/vircadia/",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Vircadia`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
